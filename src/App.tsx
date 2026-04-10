88e7133c51ed474215a32771e6ab04d828080998
praxodigital-netizen <praxodigital@gmail.com>
2026-04-10 12:44:07 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -575,8 +575,8 @@ function ServicesSection() {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {[
   { img: IMAGES.mriRoom, label: 'MRI Machine', imageClass: 'object-center' },
-  { img: IMAGES.ctScanRoom, label: 'CT Scan', imageClass: 'object-[center_55%]' },
-  { img: IMAGES.opgRoom, label: 'OPG (Digital Panoramic X-Ray)', imageClass: 'object-[center_55%]' },
+  { img: IMAGES.ctScanRoom, label: 'CT Scan', imageClass: 'object-[center_25%]' },
+  { img: IMAGES.opgRoom, label: 'OPG (Digital Panoramic X-Ray)', imageClass: 'object-[center_20%]' },
 ].map((item, i) => (
   <div key={i} className="relative rounded-2xl overflow-hidden group h-56">
     <img
